0fcc1edb5e03cdfd3bfc0cc6bf02056271854876
eidens <eidens@users.noreply.github.com>
2015-02-01 23:06:53 +0100

    test cases for basic functions

--- src/driving_cycle/core.clj
+++ src/driving_cycle/core.clj
@@ -5,7 +5,7 @@
 
 
 
-(defn ^:dynamic inside
+(defn ^:dynamic inside-bounds
   [item lower upper]
   (= true ; necessary? can both even be false? 
      (<= lower item)
@@ -14,7 +14,7 @@
 
 (defn ^:dynamic filter-outside
   [lower upper coll]
-  (filter (fn [item] (inside item lower upper))
+  (filter (fn [item] (inside-bounds item lower upper))
           coll)
   )
 
@@ -45,7 +45,10 @@
 (defn ^:dynamic remember-last-result
   [function initial]
   (let [prev (ref initial)] ; prev is result of the previous invocation (ref for thread safety)
-    (fn [] (dosync (alter prev function))))
+    (fn []
+      (println "dosync, prev: " prev)
+      (dosync (alter prev function))
+      (deref prev)))
   )
 
 
@@ -62,21 +65,8 @@
 
 (defn -main
   [& args]
-;  (let [training-data (build-from-coll args)]
-;    (println training-data)
-;    (println (take 10 (generate-walk training-data)))
-;  )
-  (dotrace [range-around] 
-           (range-around 10))
-  (dotrace [inside] 
-           (inside 9 9 11))
-  (dotrace [filter-outside] 
-           (filter-outside 10 20 (range 0 30)))
-  (dotrace [rand-item]
-           (rand-item (range 0 10)))
-  (println (dotrace [next-item]
-                    ((next-item 0 10) 0)))
-  (println (dotrace [rand-cycle]
-                    ((rand-cycle 0 10))))
-  )
-
+  (let [training-data (build-from-coll args)]
+    (println training-data)
+    (println (take 10 (generate-walk training-data)))
+    )
+)
---
+++ test/driving_cycle/core_test.clj
@@ -0,0 +1,35 @@
+(ns driving-cycle.core-test
+  (:require [clojure.test :refer :all]
+            [driving-cycle.core :refer :all]))
+
+(deftest inside-bounds-test
+  (testing "the basics"
+    (is (= true (inside-bounds 4 3 5)) "number within bounds")
+    (is (= false (inside-bounds 1 3 5)) "number below lower bound")
+    (is (= false (inside-bounds 6 3 5)) "number above upper bound")
+    )
+  (testing "edge cases"
+    (is (= true (inside-bounds 3 3 5)) "number on lower bound")
+    (is (= true (inside-bounds 5 3 5)) "number on upper bound")
+    )
+  )
+
+(deftest filter-outside-test
+  (testing "the basics"
+    (is (= '(5 6) (filter-outside 0 10 '(5 6))) "nothing to filter")
+    (is (= '(6) (filter-outside 0 10 '(6 12))) "filter value above upper bound")
+    (is (= '(6) (filter-outside 3 10 '(1 6))) "filter value below lower bound")
+    )
+  (testing "edge cases"
+    (is (= '(5 6) (filter-outside 5 6 '(5 6))) "values on bounds")
+    (is (= '() (filter-outside 0 10 '(61 12))) "all values above upper bound")
+    (is (= '() (filter-outside 10 20 '(1 6))) "all values below lower bound")
+    )
+  )
+
+(deftest range-around-test
+  (testing "the basics"
+    (is (= '(1 2 3) (range-around 2)) "basic example")
+    (is (= '(1999 2000 2001) (range-around 2000)) "basic example with slightly higher numbers")
+    )
+  )
