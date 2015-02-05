1305521cc0366fdb734c744982634973df388cc3
eidens <eidens@github>
2015-02-05 20:53:14 +0100

    added tests for core/generate

--- test/driving_cycle/core_test.clj
+++ test/driving_cycle/core_test.clj
@@ -8,7 +8,14 @@
           testfunc (proxy-with-prev-result func 1)]
       (is (= -1 (testfunc)) "first invocation: 1 * -1")
       (is (= 1 (testfunc)) "second invocation: 1 * -1 * 1")
-      (is (= -1 (testfunc)) "second invocation: 1 * -1 * 1 * -1")
-     )
-    )
-  )
+      (is (= -1 (testfunc)) "second invocation: 1 * -1 * 1 * -1"))))
+
+(deftest generate-test
+  (testing "generation of a lazy sequence"
+    (let [add-one #(+ 1 %)
+          same-number #(+ 0 %)]
+      (is (= (take 100 (generate add-one 0))
+             (range 1 101 1))
+          "should add 1 to the previous number")
+      (is (every? #(= 3 %) (take 100 (generate same-number 3)))
+          "should always be the same number"))))
