e7eb9f3b794b8be0f7efa3407146ab562c5831b8
eidens <eidens@github>
2015-02-05 20:17:11 +0100

    ":refer :all" in test files instead of ":as name"
    test files mirror src files anyway, no need to be that explicit

--- test/driving_cycle/core_test.clj
+++ test/driving_cycle/core_test.clj
@@ -1,14 +1,14 @@
 (ns driving-cycle.core-test
-  (:require [clojure.test :as test]
-            [driving-cycle.core :as core]))
+  (:require [clojure.test :refer :all]
+            [driving-cycle.core :refer :all]))
 
 (deftest proxy-with-prev-result-test
   (testing "stateful functions"
     (let [func (fn [prev] (* prev -1))
-          testfunc (core/proxy-with-prev-result func 1)]
-      (test/is (= -1 (testfunc)) "first invocation: 1 * -1")
-      (test/is (= 1 (testfunc)) "second invocation: 1 * -1 * 1")
-      (test/is (= -1 (testfunc)) "second invocation: 1 * -1 * 1 * -1")
+          testfunc (proxy-with-prev-result func 1)]
+      (is (= -1 (testfunc)) "first invocation: 1 * -1")
+      (is (= 1 (testfunc)) "second invocation: 1 * -1 * 1")
+      (is (= -1 (testfunc)) "second invocation: 1 * -1 * 1 * -1")
      )
     )
   )
--- test/driving_cycle/drunkwalk_test.clj
+++ test/driving_cycle/drunkwalk_test.clj
@@ -1,31 +1,31 @@
 (ns driving-cycle.drunkwalk-test
-  (:require [clojure.test :as test]
-            [driving-cycle.drunkwalk :as drunkwalk]))
+  (:require [clojure.test :refer :all]
+            [driving-cycle.drunkwalk :refer :all]))
 
 (deftest filter-outside-test
   (testing "the basics"
-    (test/is (= '(5 6) (drunkwalk/filter-outside 0 10 '(5 6)))
-             "nothing to filter")
-    (test/is (= '(6) (drunkwalk/filter-outside 0 10 '(6 12)))
-             "filter value above upper bound")
-    (test/is (= '(6) (drunkwalk/filter-outside 3 10 '(1 6)))
-             "filter value below lower bound")
+    (is (= '(5 6) (filter-outside 0 10 '(5 6)))
+        "nothing to filter")
+    (is (= '(6) (filter-outside 0 10 '(6 12)))
+        "filter value above upper bound")
+    (is (= '(6) (filter-outside 3 10 '(1 6)))
+        "filter value below lower bound")
     )
   (testing "edge cases"
-    (test/is (= '(5 6) (drunkwalk/filter-outside 5 6 '(5 6)))
-             "values on bounds")
-    (test/is (= '() (drunkwalk/filter-outside 0 10 '(61 12)))
-             "all values above upper bound")
-    (test/is (= '() (drunkwalk/filter-outside 10 20 '(1 6)))
-             "all values below lower bound")
+    (is (= '(5 6) (filter-outside 5 6 '(5 6)))
+        "values on bounds")
+    (is (= '() (filter-outside 0 10 '(61 12)))
+        "all values above upper bound")
+    (is (= '() (filter-outside 10 20 '(1 6)))
+        "all values below lower bound")
     )
   )
 
 (deftest range-around-test
   (testing "the basics"
-    (test/is (= '(1 2 3) (range-around 2))
-             "basic example")
-    (test/is (= '(1999 2000 2001) (range-around 2000))
-             "basic example with slightly higher numbers")
+    (is (= '(1 2 3) (range-around 2))
+        "basic example")
+    (is (= '(1999 2000 2001) (range-around 2000))
+        "basic example with slightly higher numbers")
     )
   )
