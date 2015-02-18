c3125f992c62b795d41bf5ba17da6ae715619c9f
eidens <eidens@github>
2015-02-18 23:02:15 +0100

    added test cases for creation of markov matrices from more complex input

--- test/driving_cycle/markov_test.clj
+++ test/driving_cycle/markov_test.clj
@@ -2,10 +2,27 @@
   (:require [clojure.test :refer :all]
             [driving-cycle.markov :refer :all]))
 
-(deftest markov-matrix-test
+(deftest basic-markov-matrix-test
   (testing "generation of a basic markov matrix"
-    (let [walk '(0 1 0 )
+    (let [walk '(0 1 0)
           matrix {'(0) {1 1}
                   '(1) {0 1}}]
+      (is (= matrix (gen-matrix 1 walk))
+          "markov matrix should be correctly generated"))))
+
+(deftest complex-markov-matrix-test
+  (testing "generation of a slightly more complex markov matrix"
+    (let [walk '(0 1 0 2 1 1 2 3)
+          matrix {'(0) {1 1, 2 1}
+                  '(1) {0 1, 1 1, 2 1}
+                  '(2) {1 1, 3 1}}]
+      (is (= matrix (gen-matrix 1 walk))
+          "markov matrix should be correctly generated"))))
+
+(deftest non-numbers-markov-matrix-test
+  (testing "generation of a markov matrix from non-numeric input"
+    (let [walk '((0 0) {1 1} (0 0) 1)
+          matrix {'((0 0)) {{1 1} 1, 1 1}
+                  '({1 1}) {'(0 0) 1}}]
       (is (= matrix (gen-matrix 1 walk))
           "markov matrix should be correctly generated"))))
