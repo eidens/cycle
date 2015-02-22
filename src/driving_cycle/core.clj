eae0ba8e6cc744470c18b1277857ad48001660f5
eidens <eidens@github>
2015-02-22 21:30:05 +0100

    updated core.clj with new ns

--- src/driving_cycle/core.clj
+++ src/driving_cycle/core.clj
@@ -1,13 +1,15 @@
 (ns driving-cycle.core
-  (:require [driving-cycle.drunkwalk :as walk]
+  (:require [driving-cycle.walk]
+            [driving-cycle.drunkwalk]
+            [driving-cycle.drivewalk]
+            [driving-cycle.markov-frequencies]
             [markov.core :as markov]))
 
 (defn -main
   [& args]
-  (let [training-data (take 1000 (walk/drunk-walk 0 100))
+  (let [training-data (take 1000 (driving-cycle.drunkwalkx/drunk-walk 0 100))
         matrix (markov/build-from-coll training-data)
         walk (markov/generate-walk matrix)]
     (println training-data)
     (println matrix)
-    (println (take 1000 walk))
-    ))
+    (println (take 1000 walk))))
