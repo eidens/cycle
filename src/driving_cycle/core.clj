d398ae34c7ff77553282d9962372dd516dc13192
eidens <eidens@github>
2015-03-03 01:23:50 +0100

    generate freq matrix in parallel

--- src/driving_cycle/core.clj
+++ src/driving_cycle/core.clj
@@ -7,7 +7,8 @@
             [markov.core :as markov]
             [clojure.tools.cli :as cli]
             [clojure.java.io :as io]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [clojure.math.numeric-tower :as math]))
 
 (def cli-options
   [["-f" "--outfile OUTFILE"
@@ -17,36 +18,70 @@
    ["-l" "--walk-length LENGTH"
     :id :walk-length
     :default 10
+    :parse-fn #(Integer/parseInt %)]
+   ["-o" "--markov-order ORDER"
+    :id :markov-order
+    :default 1
+    :parse-fn #(Integer/parseInt %)]
+   ["-p" "--parallel NUM"
+    :id :parallel
+    :default 1
     :parse-fn #(Integer/parseInt %)]])
 
-(defn- to-file [output-file seq]
+(defn- write-seq [output-file seq]
   (log/info "writing data to file" output-file)
   (time (with-open [wrtr (io/writer output-file)]
           (doseq [item seq]
             (.write wrtr (str item))
             (.newLine wrtr)))))
 
-(defn- gen-drivewalk [options]
+(defn- read-seq [input-file]
+  (log/info "reading sequence from file" input-file)
+  (time (with-open [rdr (io/reader input-file)]
+          (reduce conj [] (map read-string (line-seq rdr))))))
+
+(defn- generate-walk [length]
+  (log/info "generating drive-walk with" length "data points")
+  (time (take length (driving-cycle.drivewalk/drive-walk))))
+
+(defn- drivewalk [options]
   (let [output-file (:output-file options)
         walk-length (:walk-length options)
-        walk (take walk-length (driving-cycle.drivewalk/drive-walk))]
-    (log/info "generating drive-walk with" walk-length "data points")
+        walk (generate-walk walk-length)]
     (if output-file
-      (to-file output-file walk)
+      (write-seq output-file walk)
       (println walk))))
 
-(defn- gen-markov-frequencies [options]
+(defn- generate-frequencies [order walk]
+  (log/info "generating frequency matrix with order" order)
+  (time (driving-cycle.markov-frequencies/matrix order walk)))
+
+(defn- merge-frequencies
+  [map other-map]
+  (merge-with (partial merge-with +) map other-map))
+
+(defn- parallel-frequencies [order walk partition-size]
+  (log/info "using pmap for frequency generation with partition size of " partition-size)
+  (let [partitioned-walk (partition partition-size walk)
+        matrices (pmap (partial generate-frequencies order) partitioned-walk)]
+    (log/info "merging frequency matrices")
+    (time (reduce merge-frequencies matrices))))
+
+(defn- markov-frequencies [options]
   (let [output-file (:output-file options)
         input-file (:input-file options)
         walk-length (:walk-length options)
+        markov-order (:markov-order options)
+        parallel (:parallel options)
+        partition-size (max (math/round (/ walk-length parallel)) 1000)
         walk (if input-file
-               (with-open [rdr (clojure.java.io/reader input-file)]
-                 (log/info "generating markov frequencies with data from file" input-file)
-                 (reduce conj [] (map read-string (line-seq rdr))))
-               (take walk-length (driving-cycle.drivewalk/drive-walk)))
-        matrix (driving-cycle.markov-frequencies/matrix 1 walk)]
+               (read-seq input-file)
+               (generate-walk walk-length))
+        matrix (if (> parallel 1)
+                 (time (parallel-frequencies markov-order walk partition-size))
+                 (generate-frequencies markov-order walk))]
     (if output-file
-      (to-file output-file matrix)
+      (write-seq output-file matrix)
       (println matrix))))
 
 (defn -main
@@ -55,5 +90,5 @@
         options (:options cli-args)
         command (first (:arguments cli-args))]
     (case command
-      "drivewalk" (gen-drivewalk options)
-      "frequencies" (gen-markov-frequencies options))))
+      "drivewalk" (drivewalk options)
+      "frequencies" (markov-frequencies options))))
